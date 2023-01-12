e9a6e052c76f6fa3ce2abd7cb61109e515b27d1d
Nhu Viet Nguyen <braveryandglory@gmail.com>
2023-01-12 14:04:53 +0100

    init commit

--- main.py
+++ main.py
@@ -5,34 +5,33 @@ import time
 import concurrent.futures
 
 run_id = sys.stdin.readline()
-s = True
-old_time = time.time()
 
 
 def collector():
-    for i in range(40):
-        time.sleep(15)
-        save_logs(i)
+    time.sleep(15)
+    save_logs(1)
 
 
 def save_logs(t):
     print('saving logs for iteration ', t)
+    k = ['bridges', 'lights', 'lights-nodas']
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for i in range(100):
-            executor.submit(record_log, i)
+        # range here is how much pods per group we have
+        for i in k:
+            if i == 'lights-nodas':
+                for j in range(20):
+                    executor.submit(record_log, i, j)
+            else:
+                for j in range(100):
+                    executor.submit(record_log, i, j)
 
 
-def record_log(i):
-    f = open('out-' + str(i) + '.json', 'a')
-    c = 'tg-celestia-' + str(run_id.split('\n')[0]) + '-validators-' + str(i) + ''
+def record_log(i, k):
+    f = open(str(k) + '-' + str(i) + '.log', 'w')
+    c = 'tg-celestia-' + str(run_id.split('\n')[0]) + '-' + str(i) + '-' + str(k) + ''
     subprocess.call(
-        ['kubectl', 'exec', c, '--', 'cat', '/.celestia-app/data/mempool_metrics.json'],
+        ['kubectl', 'logs', c],
         stdout=f)
 
 
-while s:
-    current_time = time.time()
-    if current_time - old_time < 240:
-        collector()
-    else:
-        s = False
+collector()
